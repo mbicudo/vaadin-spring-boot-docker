f7c3e50b3e1104eaddb79cdfb4310bcd5601f472
root <root@docker0.mlab.loc>
2018-11-28 17:42:40 +0100

    docker build successful

--- src/main/java/com/gwidgets/demo/CarRepository.java
+++
@@ -1,6 +0,0 @@
-package com.gwidgets.demo;
-
-import com.gwidgets.demo.model.Car;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarRepository extends JpaRepository<Car, String> { }
